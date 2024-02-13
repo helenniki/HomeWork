c75bbfec95a57c187711a36f4ceef91acd9c3dcd
Lena Nik <pena_lena99@mail.ru>
2024-02-13 21:36:07 +0300

    initial commit

---
+++ task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке 
+//  от M до N. Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите начальную цифру: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечную цифру: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void ShowNumbers(int M, int N)
+{
+if (M==N)
+{
+    Console.Write(M);
+    return;
+}
+Console.Write(M + " ");
+ShowNumbers(M + 1, N);
+
+}
+ShowNumbers(M, N);
---
+++ task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу вычисления функции 
+// Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Write("Введите начальную цифру: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечную цифру: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FunctionAckermana ( int m, int n)
+{
+if (m == 0)
+{
+    return (n+1);
+}
+else if (n == 0)
+{
+    return FunctionAckermana(m-1,1);
+}
+else 
+{
+    return FunctionAckermana(m-1, FunctionAckermana(m, n-1));
+}
+}
+
+Console.Write (FunctionAckermana(m, n));
